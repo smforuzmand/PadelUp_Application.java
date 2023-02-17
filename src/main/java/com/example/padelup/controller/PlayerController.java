4f77c0fbac59035a913555fa5c84026fd40cc0d9
smforuzmand <career.forouzmand@gmail.com>
2023-02-17 22:39:23 +0100

    some refactore to simplify

--- src/main/java/com/example/padelup/controller/PlayerController.java
+++ src/main/java/com/example/padelup/controller/PlayerController.java
@@ -5,53 +5,48 @@ import com.example.padelup.entity.Player;
 import com.example.padelup.service.PlayerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path="/api/v1/player")
+@RequestMapping("/api/v1/player")
 public class PlayerController {
 
    private final PlayerService playerService;
-   public PlayerController(PlayerService playerService){
+
+   public PlayerController(PlayerService playerService) {
       this.playerService = playerService;
    }
 
    @PostMapping
-   public ResponseEntity<Player> create(@RequestBody final Player Player) {
-      return ResponseEntity.status(HttpStatus.CREATED).body(playerService.create(Player));
+   public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
+      Player savedPlayer = playerService.create(player);
+      return ResponseEntity.status(HttpStatus.CREATED).body(savedPlayer);
    }
 
-   @PutMapping(path = "/{id}")
-   public ResponseEntity<Player> update(@PathVariable("id") final Integer pId,
-                                        @RequestBody final Player Player) {
-      return ResponseEntity.status(HttpStatus.CREATED).body(playerService.update(pId, Player));
+   @PutMapping("/{id}")
+   public ResponseEntity<Player> updatePlayer(@PathVariable("id") int id, @RequestBody Player player) {
+      Player updatedPlayer = playerService.update(id, player);
+      return ResponseEntity.status(HttpStatus.OK).body(updatedPlayer);
    }
-   @DeleteMapping(path = "/{id}")
-   public ResponseEntity<ResultResp> deletePlayer(@PathVariable("id") final Integer pId) {
 
-      playerService.delete(pId);
-
-      final ResultResp result = new ResultResp("Player deleted!");
-
+   @DeleteMapping("/{id}")
+   public ResponseEntity<ResultResp> deletePlayer(@PathVariable("id") int id) {
+      playerService.delete(id);
+      ResultResp result = new ResultResp("Player deleted!");
       return ResponseEntity.ok(result);
    }
+
    @GetMapping("/{id}")
-   public ResponseEntity<Player> findById(@PathVariable("id") final Integer id){
-      return ResponseEntity.ok(playerService.findById(id));
+   public ResponseEntity<Player> getPlayerById(@PathVariable("id") int id) {
+      Player player = playerService.findById(id);
+      return ResponseEntity.ok(player);
    }
 
    @GetMapping
-   public ResponseEntity<List<Player>> findAll(){
-      return ResponseEntity.ok(playerService.findAll());
+   public ResponseEntity<List<Player>> getAllPlayers() {
+      List<Player> players = playerService.findAll();
+      return ResponseEntity.ok(players);
    }
-
 }
